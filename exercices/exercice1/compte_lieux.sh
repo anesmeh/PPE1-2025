10f50725135798fd614a66ab5d021195b6493ed2
Anès Meh <anesmeh7@gmail.com>
2025-10-14 20:51:46 +0200

    ajout aussi de compte_lieux premiere version

--- exercices/exercice1/compte_lieux.sh
+++ exercices/exercice1/compte_lieux.sh
@@ -4,5 +4,10 @@ CHEMIN=$1
 ANNEE=$2
 #MOIS = $3
 #NB_LIEUX = $3
-echo "heheh"
-cat $CHEMIN/$ANNEE/* | sort -k5,5 | awk '{ if ($2 == "Location") print $5 }' | uniq -c >> lieux.txt
+echo "fichier lieux.txt créé dans le répertoire courant"
+cat "$CHEMIN/$ANNEE"/* \
+  | awk '{ if ($2 == "Location") print $5 }' \
+  | sort -k1,1 \
+  | uniq -c \
+  | sort -n -r -k1,1 \
+  > lieux.txt
